769cfeb5a51f98b80f8fa3bd0c487fa2b5101e69
krishna sonawane <krishna.sonawane03@gmail.com>
2021-10-20 12:36:30 +0530

    php page

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['submit'])){
+    $mailto = "krishna.sonawane03@gmail.com";
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = "Website" . $_POST['subject'];
+    $message = "Mail received from website" . "\n\n" . $_POST['message'];
+    $headers = "From:" . $from;
+    $result = mail($mailto, $subject, $message, $headers);
+
+    if($result){
+        echo '<script type = "text/javascript">alert("Email Sent Sucessfully")</script>';
+    }else{
+        echo '<script type = "text/javascript">alert("Ty Again Later")</script>';
+    }
+}
+?>
